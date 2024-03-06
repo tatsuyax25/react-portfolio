907bbf042bc9ab05f5068ad52d52dd3b58bc42a4
Miguel Urena <urena_miguel82@outlook.com>
2024-03-06 14:59:33 -0800

    Working on Navbar

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <a href="/">Portfolio</a>
+      <div>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
