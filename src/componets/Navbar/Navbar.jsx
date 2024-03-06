11be38d95f48c7df6d5bd653db594d75b445c186
Miguel Urena <urena_miguel82@outlook.com>
2024-03-06 15:04:16 -0800

    Add class to Navbar

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import styles from './Navbar.module.css'
 export const Navbar = () => {
   return (
-    <nav>
-      <a href="/">Portfolio</a>
-      <div>
-        <ul>
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">Portfolio</a>
+      <div className={styles.menu}>
+        <ul className={styles.items}>
           <li>
             <a href="#about">About</a>
           </li>
