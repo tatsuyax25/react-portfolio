77230eed5b7c2b27a2285424910f4ee47ecfbd2b
Miguel Urena <urena_miguel82@outlook.com>
2024-03-07 13:40:20 -0800

    menu items open/close function

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
       <a className={styles.title} href="/">Portfolio</a>
       <div className={styles.menu}>
         <img className={styles.menuBtn} src={menuOpen ? getImageUrl('nav/closeIcon.png') : getImageUrl('nav/menuIcon.png')} alt='menu-button' onClick={() => setMenuOpen(!menuOpen)}/>
-        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
           <li>
             <a href="#about">About</a>
           </li>
