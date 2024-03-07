d3c01a38c0c14439fbb2b1bf1fa4cb8938a99042
Miguel Urena <urena_miguel82@outlook.com>
2024-03-07 15:02:02 -0800

    Creating Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
+import { Hero } from './componets/Hero/Hero';
 import { Navbar } from './componets/Navbar/Navbar';
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   )
 }
