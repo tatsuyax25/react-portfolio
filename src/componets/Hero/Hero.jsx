1fc32362c0e710c5f47a16efdbb74ed699701738
Miguel Urena <urena_miguel82@outlook.com>
2024-03-07 15:22:05 -0800

    Worked on Hero componet

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
+import styles from './Hero.module.css';
+import { getImageUrl } from '../../utils';
+
 export const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <section>
+      <div>
+        <h1>Hi, I&apos;m Miguel</h1>
+        <p>I&apos;m a Software Engineer from Anaheim, California!</p>
+        <a href="mailto:urena_miguel@outlook.com">Contact Me</a>
+      </div>
+      <img src={getImageUrl("hero/heroImage.jpg")} alt="Hero image of me" />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
 }
