11c353d858d2a81457cd0f453ea104ff5c3e5ea3
Miguel Urena <urena_miguel82@outlook.com>
2024-03-07 16:57:52 -0800

    Style title

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -8,10 +8,18 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I&apos;m Miguel</h1>
-        <p className={styles.description}>I&apos;m a Software Engineer from Anaheim, California!</p>
-        <a href="mailto:urena_miguel@outlook.com" className={styles.contactBtn}>Contact Me</a>
+        <p className={styles.description}>
+          I&apos;m a Software Engineer from Anaheim, California!
+        </p>
+        <a href="mailto:urena_miguel@outlook.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
-      <img src={getImageUrl("hero/heroImage.jpg")} alt="Hero image of me" className={styles.heroImg} />
+      <img
+        src={getImageUrl("hero/heroImage.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
