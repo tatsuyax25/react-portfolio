46f688eaad0c67d4b3a196d2ac38d14b9a6405e9
Miguel Urena <urena_miguel82@outlook.com>
2024-03-07 16:18:44 -0800

    Style container

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -5,13 +5,13 @@ import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I&apos;m Miguel</h1>
-        <p>I&apos;m a Software Engineer from Anaheim, California!</p>
-        <a href="mailto:urena_miguel@outlook.com">Contact Me</a>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I&apos;m Miguel</h1>
+        <p className={styles.description}>I&apos;m a Software Engineer from Anaheim, California!</p>
+        <a href="mailto:urena_miguel@outlook.com" className={styles.contactBtn}>Contact Me</a>
       </div>
-      <img src={getImageUrl("hero/heroImage.jpg")} alt="Hero image of me" />
+      <img src={getImageUrl("hero/heroImage.jpg")} alt="Hero image of me" className={styles.heroImg} />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
